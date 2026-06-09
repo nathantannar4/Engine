7f760241ed85e2997c60065a7ce52afb0e6d9cc5
Nathan Tannar <nathantannar4@gmail.com>
2026-06-09 11:44:24 -0700

    2.10.1

--- Sources/Engine/Sources/ViewControllerAdapter.swift
+++ Sources/Engine/Sources/ViewControllerAdapter.swift
@@ -16,7 +16,7 @@ open class ViewControllerAdapter<
 
     public var viewController: PlatformViewController!
 
-    var context: Any!
+    var context: Any! // Context<Coordinator>
     var conformance: ProtocolConformance<PlatformViewControllerRepresentableProtocolDescriptor>? = nil
 
     public init(
@@ -99,42 +99,138 @@ open class ViewControllerAdapter<
 
     // MARK: - ViewRepresentable
 
-    private struct ContextLayout<Coordinator> {
-        // Only `UIViewRepresentable` uses V4
-        struct V4 {
-            struct RepresentableContextValues {
-                enum EnvironmentStorage {
-                    case eager(EnvironmentValues)
-                    case lazy(() -> EnvironmentValues)
-                }
-                var preferenceBridge: AnyObject?
-                var transaction: Transaction
-                var environmentStorage: EnvironmentStorage
+    private enum RepresentableContextValues {
+        enum EnvironmentStorage {
+            case eager(EnvironmentValues)
+            case lazy(() -> EnvironmentValues)
+        }
 
-                var environment: EnvironmentValues {
-                    get {
-                        switch environmentStorage {
-                        case .eager(let environment):
-                            return environment
-                        case .lazy(let block):
-                            return block()
-                        }
+        struct Platform {
+            var rawValue: UInt8
+        }
+
+        struct V8 {
+            var preferenceBridge: AnyObject?
+            var transaction: Transaction
+            var environmentStorage: EnvironmentStorage
+            var platform: Platform
+
+            var environment: EnvironmentValues {
+                get {
+                    switch environmentStorage {
+                    case .eager(let environment):
+                        return environment
+                    case .lazy(let block):
+                        return block()
                     }
-                    set {
-                        environmentStorage = .eager(newValue)
-                    }
+                }
+                set {
+                    environmentStorage = .eager(newValue)
                 }
             }
+        }
 
-            var values: RepresentableContextValues
+        struct V4 {
+            var preferenceBridge: AnyObject?
+            var transaction: Transaction
+            var environmentStorage: EnvironmentStorage
+
+            var environment: EnvironmentValues {
+                get {
+                    switch environmentStorage {
+                    case .eager(let environment):
+                        return environment
+                    case .lazy(let block):
+                        return block()
+                    }
+                }
+                set {
+                    environmentStorage = .eager(newValue)
+                }
+            }
+        }
+    }
+
+    private enum Context<Coordinator> {
+
+        struct V8 {
+            var values: RepresentableContextValues.V8
             var coordinator: Coordinator
         }
+        case v8(V8)
+
+        struct V4 {
+            var values: RepresentableContextValues.V4
+            var coordinator: Coordinator
+        }
+        case v4(V4)
 
         struct V1 {
             var coordinator: Coordinator
             var transaction: Transaction
             var environment: EnvironmentValues
             var preferenceBridge: AnyObject?
+        }
+        case v1(V1)
+
+        var environment: EnvironmentValues {
+            get {
+                switch self {
+                case .v8(let v8):
+                    return v8.values.environment
+                case .v4(let v4):
+                    return v4.values.environment
+                case .v1(let v1):
+                    return v1.environment
+                }
+            }
+            set {
+                switch self {
+                case .v8(var v8):
+                    v8.values.environment = newValue
+                    self = .v8(v8)
+                case .v4(var v4):
+                    v4.values.environment = newValue
+                    self = .v4(v4)
+                case .v1(var v1):
+                    v1.environment = newValue
+                    self = .v1(v1)
+                }
+            }
+        }
+
+        var coordinator: Coordinator {
+            switch self {
+            case .v8(let v8):
+                return v8.coordinator
+            case .v4(let v4):
+                return v4.coordinator
+            case .v1(let v1):
+                return v1.coordinator
+            }
+        }
+
+        func asPlatformViewControllerRepresentableContext<T>(to _: T.Type) -> T {
+            let context: V1
+            switch self {
+            case .v8(let v8):
+                context = .init(
+                    coordinator: v8.coordinator,
+                    transaction: v8.values.transaction,
+                    environment: v8.values.environment,
+                    preferenceBridge: v8.values.preferenceBridge
+                )
+            case .v4(let v4):
+                context = .init(
+                    coordinator: v4.coordinator,
+                    transaction: v4.values.transaction,
+                    environment: v4.values.environment,
+                    preferenceBridge: v4.values.preferenceBridge
+                )
+            case .v1(let v1):
+                context = v1
+            }
+            return unsafeBitCast(context, to: T.self)
         }
     }
 
@@ -151,11 +247,11 @@ open class ViewControllerAdapter<
             else {
                 if let context = adapter.context, let viewController = adapter.viewController as? _Content.PlatformViewControllerType {
                     func project<T>(_ value: T) {
-                        let coordinator = unsafeBitCast(value, to: _Content.Context.self).coordinator
+                        let ctx = unsafeBitCast(value, to: Context<_Content.Coordinator>.self)
                         #if os(iOS) || os(tvOS) || os(visionOS)
-                        _Content.dismantleUIViewController(viewController, coordinator: coordinator)
+                        _Content.dismantleUIViewController(viewController, coordinator: ctx.coordinator)
                         #elseif os(macOS)
-                        _Content.dismantleNSViewController(viewController, coordinator: coordinator)
+                        _Content.dismantleNSViewController(viewController, coordinator: ctx.coordinator)
                         #endif
                     }
                     _openExistential(context, do: project)
@@ -164,34 +260,42 @@ open class ViewControllerAdapter<
             }
             if adapter.context == nil {
                 let coordinator = content.makeCoordinator()
-                let context: ContextLayout<_Content.Coordinator>.V1
-                if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *),
-                    let values = try? swift_getFieldValue("values", ContextLayout<Representable.Coordinator>.V4.RepresentableContextValues.self, bridgedContext)
+                let context: Context<_Content.Coordinator>
+                if #available(iOS 27.0, macOS 27.0, tvOS 27.0, watchOS 27.0, visionOS 27.0, *),
+                    let values = try? swift_getFieldValue("values", RepresentableContextValues.V8.self, bridgedContext)
                 {
-                    context = ContextLayout<_Content.Coordinator>.V1(
-                        coordinator: coordinator,
-                        transaction: values.transaction,
-                        environment: values.environment,
-                        preferenceBridge: values.preferenceBridge
+                    context = .v8(
+                        Context<_Content.Coordinator>.V8(
+                            values: values,
+                            coordinator: coordinator
+                        )
+                    )
+                } else if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *),
+                    let values = try? swift_getFieldValue("values", RepresentableContextValues.V4.self, bridgedContext)
+                {
+                    context = .v4(
+                        Context<_Content.Coordinator>.V4(
+                            values: values,
+                            coordinator: coordinator
+                        )
                     )
                 } else {
-                    let bridgedContext = unsafeBitCast(
-                        bridgedContext,
-                        to: ContextLayout<Representable.Coordinator>.V1.self
-                    )
-                    context = ContextLayout<_Content.Coordinator>.V1(
-                        coordinator: coordinator,
-                        transaction: bridgedContext.transaction,
-                        environment: bridgedContext.environment,
-                        preferenceBridge: bridgedContext.preferenceBridge
+                    let bridgedContext = unsafeBitCast(bridgedContext, to: Context<Representable.Coordinator>.V1.self)
+                    context = .v1(
+                        Context<_Content.Coordinator>.V1(
+                            coordinator: coordinator,
+                            transaction: bridgedContext.transaction,
+                            environment: bridgedContext.environment,
+                            preferenceBridge: bridgedContext.preferenceBridge
+                        )
                     )
                 }
-                adapter.context = unsafeBitCast(context, to: _Content.Context.self)
+                adapter.context = context
             }
             func project<T>(_ value: T) -> _Content.Context {
-                var ctx = unsafeBitCast(value, to: ContextLayout<_Content.Coordinator>.V1.self)
+                var ctx = unsafeBitCast(value, to: Context<_Content.Coordinator>.self)
                 adapter.transformViewControllerEnvironment(&ctx.environment)
-                return unsafeBitCast(ctx, to: _Content.Context.self)
+                return ctx.asPlatformViewControllerRepresentableContext(to: _Content.Context.self)
             }
             let ctx = _openExistential(adapter.context!, do: project)
             if adapter.viewController == nil {
