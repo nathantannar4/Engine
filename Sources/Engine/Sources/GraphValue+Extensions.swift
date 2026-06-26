8015f133c7740aac703eb1238888f1679aea2c96
Danqing Liu <git@danqing.io>
2026-06-26 18:24:58 -0400

    Update GraphValue+Extensions.swift

--- Sources/Engine/Sources/GraphValue+Extensions.swift
+++ Sources/Engine/Sources/GraphValue+Extensions.swift
@@ -7,7 +7,12 @@ import SwiftUI
 
 @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
 private typealias GraphValueUnsafeBitCastFunction<A, B> =
-    @convention(thin) (_GraphValue<A>, B.Type) -> _GraphValue<B>
+    @convention(thin) (
+        _GraphValue<A>,
+        B.Type,
+        UnsafeRawPointer,
+        UnsafeRawPointer
+    ) -> _GraphValue<B>
 
 @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
 nonisolated(unsafe) private let graphValueUnsafeBitCastSymbol = dlsym(
@@ -17,7 +22,12 @@ nonisolated(unsafe) private let graphValueUnsafeBitCastSymbol = dlsym(
 
 @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
 private typealias GraphValueUnsafeCastFunction<A, B> =
-    @convention(thin) (_GraphValue<A>, B.Type) -> _GraphValue<B>
+    @convention(thin) (
+        _GraphValue<A>,
+        B.Type,
+        UnsafeRawPointer,
+        UnsafeRawPointer
+    ) -> _GraphValue<B>
 
 @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
 nonisolated(unsafe) private let graphValueUnsafeCastSymbol = dlsym(
@@ -29,6 +39,9 @@ extension _GraphValue {
 
     func unsafeCast<T>(to _: T.Type) -> _GraphValue<T> {
         if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            let valueMetadata = unsafeBitCast(Value.self, to: UnsafeRawPointer.self)
+            let targetMetadata = unsafeBitCast(T.self, to: UnsafeRawPointer.self)
+
             if MemoryLayout<Value>.size == MemoryLayout<T>.size,
                 let symbol = graphValueUnsafeBitCastSymbol
             {
@@ -36,13 +49,13 @@ extension _GraphValue {
                     symbol,
                     to: GraphValueUnsafeBitCastFunction<Value, T>.self
                 )
-                return function(self, T.self)
+                return function(self, T.self, valueMetadata, targetMetadata)
             } else if let symbol = graphValueUnsafeCastSymbol {
                 let function = unsafeBitCast(
                     symbol,
                     to: GraphValueUnsafeCastFunction<Value, T>.self
                 )
-                return function(self, T.self)
+                return function(self, T.self, valueMetadata, targetMetadata)
             }
         }
 
